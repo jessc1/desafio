8220b3bdb7f5a176fb7ede620f3e29ce5bc68ba3
jessc1 <50178378+jessc1@users.noreply.github.com>
2025-05-12 15:23:19 -0300

    Update desafio_tecnico.py

--- desafio_tecnico.py
+++ desafio_tecnico.py
@@ -4,7 +4,7 @@ def process(INDICE: int, SOMA: int, k: int) -> int:
         SOMA = SOMA + k
     return SOMA
 
-#print(process(13, 0, 0)) # SOMA = 91
+print(process(13, 0, 0)) # SOMA = 91
 
 
 def fibonacci(n: int) -> int:
@@ -16,33 +16,27 @@ def fibonacci(n: int) -> int:
        x =  y
        y = fib   
    return fib == n 
-#print(fibonacci(4))
+print(fibonacci(4))
 
 
 
     
-def read_file():
+def read_file() -> str:
     import json
+    results = []
     with open('dados.json', 'r') as file:
         data = json.load(file)
         for row in data:
-           
-            valores = float(row["valor"])
-            
-        min_value = (min(valores))
-        max_value = max(valores)
-        print('b',min_value)
-        
-print(read_file())  
-        
-            
-            
-        
-        
-           
-            
-    
-            
+            dias = int(row["dia"])            
+            valor = float(row["valor"])
+            result = {"dia": dias, "valor": valor}
+            results.append(result)
+        min_value = min(results, key=lambda data: data['valor'])
+        max_value = max(results, key=lambda data: data['valor'])
+       
+    return f"O mínimo valor é: {min_value['valor']} e o máximo valor é: {max_value['valor']:.2f}"       
+       
+print(read_file())               
 
 def revert_string(string):
     return string[::-1]
